f61520f72d977408bb83089b8bb933f1341fc757
RobbieG <rwhgower63@gmail.com>
2023-09-16 17:13:52 +0100

    All codeWars solutions to date added to repo

--- Level 6/breakCamelCase.js
+++ Level 6/breakCamelCase.js
@@ -2,11 +2,17 @@
 function solution(string) {
 const regexSplit=/([a-z]*)([A-Z]{1}[a-z]*)/g
 
-
-
 const broken=string.replace(regexSplit,'$1 $2')
-console.log(broken)
+return broken
 
 }
 
 solution("helleMateWhat")
+
+
+//alt solutions
+// complete the function
+// function solution(string) {
+//     return(string.replace(/([A-Z])/g, ' $1'));
+  
+//   }
