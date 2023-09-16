04f18733b61817672a00bb0e0277c3589c87c8a9
RobbieG <rwhgower63@gmail.com>
2023-09-16 18:26:03 +0100

    Moved into level 5 challenges, some complete inc fibProduct

--- Level 5/productFibonacci.js
+++ Level 5/productFibonacci.js
@@ -34,3 +34,14 @@ function productFib(prod){
 }
 
   console.log(productFib(800))
+
+  //Alt solution
+//   function productFib(prod){
+//     var n = 0;
+//     var nPlus = 1;  
+//     while(n*nPlus < prod) {
+//       nPlus = n + nPlus;
+//       n = nPlus - n;
+//     }
+//     return [n, nPlus, n*nPlus===prod];
+//   }
